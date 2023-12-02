5a8410385eef02d0654493769927f042ff0b7c41
Vyasa Krishna K <101573457+VyasaK7@users.noreply.github.com>
2023-12-02 23:08:53 +0530

    Update CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://universal-cities.onrender.com";
 
 const CitiesContext = createContext();
 
